7c1c10063ec82f070786215a7779c8c2fc2a9579
Sonia Susanto <soniasusanto@gmail.com>
2020-04-19 18:07:10 -0400

    edited order of solutions

--- twoNumberSum.js
+++ twoNumberSum.js
@@ -3,7 +3,29 @@ Brute Force Solution A - while loop
 Time: O(N)
 Space: O(1)
 */
+
 function twoNumberSumA(array, targetSum) {
+  let finalArr = [];
+  for (let i = 0; i < array.length - 1; i++) {
+    let firstEl = array[i];
+
+    for (let j = i + 1; j < array.length; j++) {
+      let secondEl = array[j];
+
+      if (firstEl + secondEl === targetSum) {
+        finalArr.push(firstEl, secondEl);
+      }
+    }
+  }
+  return finalArr;
+}
+
+/*
+Brute Force Solution B - nested for loop
+Time: O(N^2)
+Space: O(1)
+*/
+function twoNumberSumB(array, targetSum) {
   let i = 0;
   let j = 1;
   let currentNum = array[i];
@@ -25,28 +47,7 @@ function twoNumberSumA(array, targetSum) {
   }
 }
 
-/* 
-Brute Force Solution B - nested for loop
-Time: O(N^2)
-Space: O(1)
-*/
-function twoNumberSumB(array, targetSum) {
-  let finalArr = [];
-  for (let i = 0; i < array.length - 1; i++) {
-    let firstEl = array[i];
-
-    for (let j = i + 1; j < array.length; j++) {
-      let secondEl = array[j];
-
-      if (firstEl + secondEl === targetSum) {
-        finalArr.push(firstEl, secondEl);
-      }
-    }
-  }
-  return finalArr;
-}
-
-/* 
+/*
 Brute Force Solution C - using difference of targetSum and each num in given array
 Time: O(N^2)
 Space: O(1)
@@ -65,8 +66,8 @@ function twoNumberSumC(array, targetSum) {
   return finalArr;
 }
 
-/* 
-Optimized Solution - using hash table to save every number in hash table, solving for each difference, 
+/*
+Optimized Solution - using hash table to save every number in hash table, solving for each difference,
 and checking if the difference is stored in the hash table.
 Time: O(N)
 Space: O(1)
